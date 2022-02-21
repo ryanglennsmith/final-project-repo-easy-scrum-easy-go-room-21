bc6099fb89807da36fa96aeae4d5a12800aef2b2
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-02-21 15:25:00 +0000

    cards created

---
+++ components/course-card/CourseCard.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Rating from '@mui/material/Rating';
+
+//plan -  fetch data from ryan's mock api
+
+//pass mock data as prop
+export default function CourseCard({ cards }) {
+  return (
+    <Container sx={{ py: 8 }} maxWidth="md">
+      {/* End hero unit */}
+      <Grid container spacing={4}>
+        {cards.map((card) => (
+          <Grid item key={card.id} xs={12} sm={6} md={4}>
+            <Card
+              sx={{
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <CardMedia
+                component="img"
+                sx={{
+                  // 16:9
+                  pt: '56.25%',
+                }}
+                image={card.image}
+                alt="random"
+              />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {card.teacher_name}
+                </Typography>
+                <Typography>{card.course_brief}</Typography>
+              </CardContent>
+              <CardActions>
+                <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
