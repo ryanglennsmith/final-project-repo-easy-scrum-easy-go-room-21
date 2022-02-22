a424b3daed81b799241be4010ff3719f5ce88742
ryanglennsmith <ryan.glenn.smith@gmail.com>
2022-02-22 10:44:14 +0000

    refactored getStaticProps/Paths

--- pages/courses/[id].js
+++ pages/courses/[id].js
@@ -1,55 +1,22 @@
 import Image from 'next/image';
 import { Button } from '@mui/material';
-import { fontWeight } from '@mui/system';
 export default function CoursePage({ data }) {
-  // data to add to dummy json file
-  // course_title:
-  // rating:
-  // dates_available: {Sunday: false, Monday: true, Tuesday: false, Wednesday: true, Thursday: true, Friday: false, Saturday: true}
-
-  const fakeData = {
-    course_id: 1,
-    teacher_name: 'Simona Mountcastle',
-    email: 'smountcastle0@ebay.co.uk',
-    location: 'Skrwilno',
-    bio_text:
-      'Small batch crucifix helvetica kickstarter messenger bag before they sold out everyday carry viral ethical af next level chillwave hammock succulents pug.  Mixtape YOLO single-origin coffee sartorial, kitsch pitchfork ugh pabst letterpress.  Sartorial wayfarers lumbersexual retro before they sold out plaid etsy chillwave chicharrones portland gastropub VHS artisan tumblr.  Typewriter shaman locavore ramps, tumeric ugh pabst.  Umami kickstarter coloring book kitsch chartreuse, ramps plaid copper mug.  Offal everyday carry intelligentsia glossier, woke deep v microdosing selvage freegan hexagon scenester.  Mlkshk listicle portland raw denim, meditation lyft hoodie mustache hashtag.',
-    long_description:
-      "Heirloom gastropub whatever cardigan neutra listicle wayfarers.  Cardigan you probably haven't heard of them four dollar toast, lumbersexual iceland affogato hexagon pabst poutine live-edge vexillologist af prism.  Man bun live-edge subway tile literally lumbersexual pug.  Hella freegan iceland small batch poke slow-carb.  Try-hard vice ennui pork belly, 90's subway tile echo park heirloom bushwick blog readymade lo-fi kogi flannel street art.  Squid farm-to-table butcher ugh heirloom direct trade.",
-    is_online: 'true',
-    is_offline: 'false',
-    image: 'https://images.unsplash.com/photo-1461344577544-4e5dc9487184',
-    course_brief:
-      'Master cleanse taiyaki ethical bushwick slow-carb migas XOXO direct trade',
-    course_title: 'Painting for idiots',
-    rating: 3.2,
-    dates_available: [
-      {
-        Sunday: false,
-      },
-      { Monday: true },
-      { Tuesday: false },
-      { Wednesday: true },
-      { Thursday: true },
-      { Friday: false },
-      { Saturday: true },
-    ],
-  };
-  const days = fakeData.dates_available.map((date) => {
+  const course = data.course[0];
+  const days = course.dates_available.map((date) => {
     return Object.keys(date);
   });
-  const available = fakeData.dates_available.map((date) => {
+  const available = course.dates_available.map((date) => {
     return Object.values(date).toString();
   });
 
   return (
     <div className="topLevelContainer courseProfile">
-      <Image src={fakeData.image} width="400px" height="260px" alt="painting" />
+      <Image src={course.image} width="400px" height="260px" alt="painting" />
       <section className="lessonCard">
-        <h1 className="courseTitle">{fakeData.course_title}</h1>
-        <p className="courseSubHead">{fakeData.course_brief}</p>
-        <p className="courseTeacherName">{fakeData.teacher_name}</p>
-        <p className="courseRating">rating: {fakeData.rating}</p>
+        <h1 className="courseTitle">{course.course_title}</h1>
+        <p className="courseSubHead">{course.course_brief}</p>
+        <p className="courseTeacherName">{course.teacher_name}</p>
+        <p className="courseRating">rating: {course.rating}</p>
         <p className="courseDates">
           {available.map((value, index) => {
             if (value == 'true') {
@@ -60,34 +27,34 @@ export default function CoursePage({ data }) {
           })}
         </p>
         <p className="courseOnlineStatus">
-          {fakeData.is_offline === 'true' ? (
+          {course.is_offline === 'true' ? (
             <span style={{ fontWeight: 'bold' }}>Offline</span>
           ) : (
             <span style={{ color: 'gray' }}>Offline</span>
           )}{' '}
           |{' '}
-          {fakeData.is_online === 'true' ? (
+          {course.is_online === 'true' ? (
             <span style={{ fontWeight: 'bold' }}>Online</span>
           ) : (
             <span style={{ color: 'gray' }}>Online</span>
           )}
         </p>
-        <Button>{fakeData.email}</Button>
+        <Button>{course.email}</Button>
       </section>
       <section className="courseDescription">
         <h2>About this class</h2>
-        <p className="courseLongDescription">{fakeData.long_description}</p>
+        <p className="courseLongDescription">{course.long_description}</p>
       </section>
 
       {/* 
 ---
 
-   - Profile (title, subtext, rating, name, date, aboutClass, content)
-         - Image (image)
-         - LessonCard(title, subText, rating, name, date)
-               - Action Button (text, function) (Child component)
-         - Description (aboutClass, content) (Child component)
- */}
+- Profile (title, subtext, rating, name, date, aboutClass, content)
+- Image (image)
+- LessonCard(title, subText, rating, name, date)
+- Action Button (text, function) (Child component)
+- Description (aboutClass, content) (Child component)
+*/}
     </div>
   );
 }
@@ -99,16 +66,61 @@ export default function CoursePage({ data }) {
 // }
 
 export async function getStaticPaths() {
-  return {
-    // call a fetch to all the courses
-    // map all courses by id
-    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+  // call a fetch to all the courses
+  // map all courses by id
+  const res = await fetch('http://localhost:3000/api/courses');
+  const data = await res.json();
+  // data.courses
+  const paths = data.courses.map((course) => {
+    const id = String(course.course_id);
+    return {
+      params: {
+        id: id,
+      },
+    };
+  });
 
+  return {
+    paths,
     fallback: false,
   };
 }
-export async function getStaticProps(stat) {
-  console.log(stat.params.id);
-  const data = { id: stat.params.id, course_name: 'hamburger cookery' };
-  return { props: { data: data } };
+export async function getStaticProps({ params }) {
+  const res = await fetch(`http://localhost:3000/api/courses/${params.id}`);
+  const data = await res.json();
+  return { props: { data } };
 }
+
+// data to add to dummy json file
+// course_title:
+// rating:
+// dates_available: {Sunday: false, Monday: true, Tuesday: false, Wednesday: true, Thursday: true, Friday: false, Saturday: true}
+
+// const fakeData = {
+//   course_id: 1,
+//   teacher_name: 'Simona Mountcastle',
+//   email: 'smountcastle0@ebay.co.uk',
+//   location: 'Skrwilno',
+//   bio_text:
+//     'Small batch crucifix helvetica kickstarter messenger bag before they sold out everyday carry viral ethical af next level chillwave hammock succulents pug.  Mixtape YOLO single-origin coffee sartorial, kitsch pitchfork ugh pabst letterpress.  Sartorial wayfarers lumbersexual retro before they sold out plaid etsy chillwave chicharrones portland gastropub VHS artisan tumblr.  Typewriter shaman locavore ramps, tumeric ugh pabst.  Umami kickstarter coloring book kitsch chartreuse, ramps plaid copper mug.  Offal everyday carry intelligentsia glossier, woke deep v microdosing selvage freegan hexagon scenester.  Mlkshk listicle portland raw denim, meditation lyft hoodie mustache hashtag.',
+//   long_description:
+//     "Heirloom gastropub whatever cardigan neutra listicle wayfarers.  Cardigan you probably haven't heard of them four dollar toast, lumbersexual iceland affogato hexagon pabst poutine live-edge vexillologist af prism.  Man bun live-edge subway tile literally lumbersexual pug.  Hella freegan iceland small batch poke slow-carb.  Try-hard vice ennui pork belly, 90's subway tile echo park heirloom bushwick blog readymade lo-fi kogi flannel street art.  Squid farm-to-table butcher ugh heirloom direct trade.",
+//   is_online: 'true',
+//   is_offline: 'false',
+//   image: 'https://images.unsplash.com/photo-1461344577544-4e5dc9487184',
+//   course_brief:
+//     'Master cleanse taiyaki ethical bushwick slow-carb migas XOXO direct trade',
+//   course_title: 'Painting for idiots',
+//   rating: 3.2,
+//   dates_available: [
+//     {
+//       Sunday: false,
+//     },
+//     { Monday: true },
+//     { Tuesday: false },
+//     { Wednesday: true },
+//     { Thursday: true },
+//     { Friday: false },
+//     { Saturday: true },
+//   ],
+// };
