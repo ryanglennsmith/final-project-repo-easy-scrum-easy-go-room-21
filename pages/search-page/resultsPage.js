19c14991315cd10f541665d6ace06d258b963d9c
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-02-25 11:05:51 +0000

    planned out search page

---
+++ pages/search-page/resultsPage.js
@@ -0,0 +1,10 @@
+//import link from next
+// put a temp hyperlink on homepage so we can navigate to the search page with ease
+//import navBar, course-card and footer from components folder
+// import api data and map through to create card content
+
+// from homepage, user input is taken in  and onclick of search button-----> user input is passed to results page  ----> list of results displayed on results page.
+
+//note to self: search bar components on both pages will operate in different ways. The search component on results page WILL NOT redirect users to another page to display results whereas it will on the homepage.
+
+//connect search button so that when it is pressed, we are redirected to the results page(which then would display course cards related to user input)
