e0cf5437fd16934933a9d69cef519bd30edb2edf
Eliana <elianacalote@gmail.com>
2022-03-07 17:35:04 +0000

    search test

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -10,13 +10,33 @@
 //     });
 //   });
 
-context('page name', () => {
+// context('Home page', () => {
+//   beforeEach(() => {
+//     cy.visit('http://localhost:3000/');
+//   });
+//   it('should takes us to home page, when we click on the WeShare logo', () => {
+//     cy.get('img[href*="/"]').click({ multiple: pass });
+
+//     cy.url().should('include', '/');
+//   });
+// });
+
+context('Search page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://localhost:3000/search/cooking');
   });
   it('should takes us to home page, when we click on the WeShare logo', () => {
-    cy.get('img[href*="/"]').click({ multiple: pass });
-
+    cy.get('img[src*="https://i.lensdump.com/i/rLRO3c.png"]').click();
     cy.url().should('include', '/');
   });
 });
+
+// context('Header ', () => {
+//       beforeEach(() => {
+//         cy.visit('http://localhost:3000/');
+//       });
+//       it('what it should do', () => {
+//         cy.get('element').contains('text');
+//       });
+//     });
+
