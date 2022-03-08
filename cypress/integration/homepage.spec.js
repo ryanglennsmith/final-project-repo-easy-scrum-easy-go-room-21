f6432808be227718451b0e66aebf7ee9b7a75046
Eliana <elianacalote@gmail.com>
2022-03-08 11:31:31 +0000

    some changes on testing

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -10,21 +10,13 @@
 //     });
 //   });
 
-// context('Home page', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:3000/');
-//   });
-//   it('should takes us to home page, when we click on the WeShare logo', () => {
-//     cy.get('img[href*="/"]').click({ multiple: pass });
 
-//     cy.url().should('include', '/');
-//   });
-// });
 
 context('Home page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
   });
+
   // it('should takes us to home page, when we click on the WeShare logo', () => {
   //   cy.get('img[src*="https://i.lensdump.com/i/rLRO3c.png"]').click();
   //   cy.url().should('include', '/');
@@ -39,13 +31,7 @@ context('Home page', () => {
       'Build, monetize, manage, and grow membership sites, courses, subscriptions, communities, events, or the digital product of your dreams'
     );
   });
+
+
 });
 
-// context('Header ', () => {
-//       beforeEach(() => {
-//         cy.visit('http://localhost:3000/');
-//       });
-//       it('what it should do', () => {
-//         cy.get('element').contains('text');
-//       });
-//     });
