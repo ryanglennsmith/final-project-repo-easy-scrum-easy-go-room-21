56216b063a7922ca2784ed1e09e89d426287e567
ryanglennsmith <ryan.glenn.smith@gmail.com>
2022-03-12 11:02:18 +0000

    updated to next/link on courseCard comp

--- components/course-card/CourseCard.js
+++ components/course-card/CourseCard.js
@@ -29,8 +29,8 @@ import {
   tagsBtn,
   tagsBtnMainpage,
 } from '../../globalCss.js';
-import { Link } from '@mui/material';
-
+import Link from 'next/link';
+import { Link as MUILink } from '@mui/material';
 //plan -  fetch data from ryan's mock api
 
 //pass mock data as prop
@@ -53,86 +53,108 @@ export default function CourseCard({ cards, setSearch }) {
           >
             <Link
               href={`/courses/${card.course_id}`}
-              style={{ textDecoration: 'none' }}
+              style={{
+                textDecoration: 'none',
+              }}
             >
-              <Card sx={courseCardCardSection}>
-                {/* courseCardCardMedia from globalCss isn't working for img  */}
+              <MUILink underline="none">
+                <Card sx={courseCardCardSection}>
+                  {/* courseCardCardMedia from globalCss isn't working for img  */}
 
-                <CardMedia
-                  component="img"
-                  sx={
-                    (courseCardCardMedia,
-                    { padding: '20px', borderRadius: '20px', height: '255px' })
-                  }
-                  image={card.images.thumb}
-                  alt={card.teacher_name}
-                />
+                  <CardMedia
+                    component="img"
+                    sx={
+                      (courseCardCardMedia,
+                      {
+                        padding: '20px',
+                        borderRadius: '20px',
+                        height: '255px',
+                      })
+                    }
+                    image={card.images.thumb}
+                    alt={card.teacher_name}
+                  />
 
-                {/* tags   */}
-                <CardActions sx={{ padding: 0, width: '325px' }}>
-                  <div
-                    className=" displayInlineBlock "
-                    data-cy="course-tutor-tags"
-                  >
-                    {card.course_tags.map((item, index) => {
-                      function onClick(e) {
-                        e.preventDefault();
-                        if (setSearch === '') {
-                          console.log('yes');
-                        } else {
-                          setSearch(item);
+                  {/* tags   */}
+                  <CardActions sx={{ padding: 0, width: '325px' }}>
+                    <div
+                      className=" displayInlineBlock "
+                      data-cy="course-tutor-tags"
+                    >
+                      {card.course_tags.map((item, index) => {
+                        function onClick(e) {
+                          e.preventDefault();
+                          if (setSearch === '') {
+                            console.log('yes');
+                          } else {
+                            setSearch(item);
+                          }
+                          router.push(`/search/${item}`);
                         }
-                        router.push(`/search/${item}`);
+
+                        return (
+                          <Box className="tagsBtnContainer" key={index}>
+                            <Button
+                              onClick={onClick}
+                              sx={{
+                                ...tagsBtnMainpage,
+                                fontSize: '10px',
+                              }}
+                            >
+                              {item}
+                            </Button>
+                          </Box>
+                        );
+                      })}
+                    </div>
+                  </CardActions>
+
+                  <CardContent sx={courseCardCardContent}>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                      className="courseCardContentTitle"
+                      sx={courseCardContentTitle}
+                    >
+                      {card.course_title}
+                    </Typography>
+
+                    <Typography
+                      className="courseCardContentTeacherName"
+                      sx={courseCardContentTeacherName}
+                    >
+                      {card.teacher_name}
+                    </Typography>
+                    <Typography
+                      className="courseCardContentCourseBrief"
+                      sx={courseCardContentCourseBrief}
+                    >
+                      {card.course_brief}
+                    </Typography>
+                  </CardContent>
+                  <CardActions sx={courseCardActions}>
+                    {/* star-rating add sx */}
+                    {/* need to resize for smaller view portr */}
+                    <Rating
+                      name="read-only"
+                      // defaultValue={Number(card.rating.toFixed(1))}
+                      defaultValue={
+                        card.reviews.length === 0
+                          ? 0
+                          : Number(
+                              (
+                                card.reviews
+                                  .map((review) => review.rating)
+                                  .reduce((a, b) => a + b) / card.reviews.length
+                              ).toFixed(1)
+                            )
                       }
-
-                      return (
-                        <Box className="tagsBtnContainer" key={index}>
-                          <Button
-                            onClick={onClick}
-                            sx={{
-                              ...tagsBtnMainpage,
-                              fontSize: '10px',
-                            }}
-                          >
-                            {item}
-                          </Button>
-                        </Box>
-                      );
-                    })}
-                  </div>
-                </CardActions>
-
-                <CardContent sx={courseCardCardContent}>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                    className="courseCardContentTitle"
-                    sx={courseCardContentTitle}
-                  >
-                    {card.course_title}
-                  </Typography>
-
-                  <Typography
-                    className="courseCardContentTeacherName"
-                    sx={courseCardContentTeacherName}
-                  >
-                    {card.teacher_name}
-                  </Typography>
-                  <Typography
-                    className="courseCardContentCourseBrief"
-                    sx={courseCardContentCourseBrief}
-                  >
-                    {card.course_brief}
-                  </Typography>
-                </CardContent>
-                <CardActions sx={courseCardActions}>
-                  {/* star-rating add sx */}
-                  {/* need to resize for smaller view portr */}
-                  <Rating
-                    name="read-only"
-                    // defaultValue={Number(card.rating.toFixed(1))}
-                    defaultValue={
+                      precision={0.1}
+                      readOnly
+                    />
+                    {/* teacher rating as a number add sx margin right */}
+                    <Typography sx={courseCardRateAvg}>{`${
                       card.reviews.length === 0
                         ? 0
                         : Number(
@@ -142,36 +164,22 @@ export default function CourseCard({ cards, setSearch }) {
                                 .reduce((a, b) => a + b) / card.reviews.length
                             ).toFixed(1)
                           )
-                    }
-                    precision={0.1}
-                    readOnly
-                  />
-                  {/* teacher rating as a number add sx margin right */}
-                  <Typography sx={courseCardRateAvg}>{`${
-                    card.reviews.length === 0
-                      ? 0
-                      : Number(
-                          (
-                            card.reviews
-                              .map((review) => review.rating)
-                              .reduce((a, b) => a + b) / card.reviews.length
-                          ).toFixed(1)
-                        )
-                  }`}</Typography>
-                  {/* number of the comments  */}
-                  <Typography
-                    className="courseCardReviewCount"
-                    sx={{ ...courseCardReviewCount }}
-                  >
-                    ({card.reviews.length})
-                  </Typography>
-                  {matches && (
-                    <Button variant="outlined" sx={courseCardButton}>
-                      More Info
-                    </Button>
-                  )}
-                </CardActions>
-              </Card>
+                    }`}</Typography>
+                    {/* number of the comments  */}
+                    <Typography
+                      className="courseCardReviewCount"
+                      sx={{ ...courseCardReviewCount }}
+                    >
+                      ({card.reviews.length})
+                    </Typography>
+                    {matches && (
+                      <Button variant="outlined" sx={courseCardButton}>
+                        More Info
+                      </Button>
+                    )}
+                  </CardActions>
+                </Card>
+              </MUILink>
             </Link>
           </Grid>
         ))}
